1f931294e0df437e3cb2abeeb55804bd661c624f
NADA MILONE <nadamilone@gmail.com>
2022-03-02 13:38:09 -0500

    commit tree 29ad022fa1b2c41551c9291a57d054a128adc848

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code goes here
+
+//expected 'JavaScript is so cool. It lets me add text to my page programmatically.' 
+//to equal 'This is really cool!'
+
+//DOMContentLoaded happens when DOM is fully parsed
+//load event happens when all CSS and JS have finsihed loading
+
+
+//first set up a DOMContentLoaded event listener
+
+//use textContent or innerHTML for this lab
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    // console.log("The DOM has loaded");
+    updateDOM()
+});
+
+// console.log( "This console.log() fires when index.js loads - before the DOMContentLoaded");
+
+function updateDOM() {
+    document.getElementById("text").innerHTML = "This is really cool!";
+}
+
+
